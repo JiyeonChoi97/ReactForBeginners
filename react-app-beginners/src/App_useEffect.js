4d20e9b5116f328495f239d76f9c18aae88dbf1a
jychoi <ski04084@naver.com>
2022-01-19 17:15:35 +0900

    1. useEffect

---
+++ react-app-beginners/src/App_useEffect.js
@@ -0,0 +1,39 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  // useEffect 예제
+  /* const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log('i run all the time');
+  useEffect(() => {
+    console.log('CALL THE API');
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR ", keyword);
+    }
+  }, [keyword]) */
+  return (
+    <div></div>
+
+// useEffect 예제
+/*     <div>
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+      {<Button text={"Continue"} />}
+    </div> */
+  );
+}
+
+export default App;
