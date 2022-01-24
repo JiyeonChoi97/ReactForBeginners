49f8ac9b7981919ebf962e670a9060f9a823fc17
jychoi <ski04084@naver.com>
2022-01-24 16:07:15 +0900

    1. React Router

--- react-app-movieapp/src/App.js
+++ react-app-movieapp/src/App.js
@@ -1,6 +1,23 @@
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import HOME from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <HOME />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
