0f0a447563e1b0ddd1ac36a14b68d9398252c26e
jychoi <ski0484@naver.com>
2022-03-06 19:50:44 +0900

    1. Actions

--- redux/vanila-redux/README.md
+++ redux/vanila-redux/README.md
@@ -1,3 +1,18 @@
 #Vanilla Redux
 
 Learning Vanilla-Redux and React-Redux
+
+- 문법
+
+    ```tsx
+    import {createStore} from "redux";
+    
+    const reducer = (data, action) => {};
+    const store = createStore(reducer);
+    ```
+
+  store : data를 넣을 수 있는 장소 생성
+
+  reducer : data modify function
+
+  action : redux에서 function을 부를때 쓰는 두번째 parameter, 혹은 argument
--- redux/vanila-redux/src/index.js
+++ redux/vanila-redux/src/index.js
@@ -4,8 +4,19 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifer = (state = 0) => {
-    return state;
+const countModifer = (state = 0, action) => {
+    if(action.type === "ADD") {
+        return state + 1;
+    } else if(action.type === "MINUS") {
+        return state - 1;
+    } else {
+        return state;
+    }
 };
 
 const countStore = createStore(countModifer);
+
+// reducer에 action 전송
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
